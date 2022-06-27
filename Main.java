ad52cb168645704a8e803ee4ee34399005ced6e9
Mallllco <map_98@hotmail.it>
2022-06-27 09:06:53 +0200

    Update Main.java
    fixed the horrible indentation

--- Main.java
+++ Main.java
@@ -22,18 +22,18 @@ public class Main {
 	}
 	
 	 public static void main (String[] args) {
-		 		String word;
-		 		Scanner input = new Scanner(System.in);
+		String word;
+		Scanner input = new Scanner(System.in);
 		 		
-		 		System.out.println("Inserisci la parola: ");
-		 		word = input.next();
+		System.out.println("Inserisci la parola: ");
+		word = input.next();
 		 		
-		 		if(Palindromo(word)) {
-		 			System.out.println("La parola è palindroma");
-		 		}else{
-		 			System.out.println("La parola non è palindroma");
-		 		}
+		if(Palindromo(word)) {
+			System.out.println("La parola Ã¨ palindroma");
+		}else{
+		 	System.out.println("La parola non Ã¨ palindroma");
+		 }
 		 		
-		 		input.close();
-	    }
+		input.close();
+	 }
 }
